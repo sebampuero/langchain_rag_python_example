192cc7e0057d6d6105ae723b8b13484d5d0b321f
Sebastian <sebastian.ampuerom@gmail.com>
2025-03-23 20:11:15 +0000

    chore: update deps

--- src/server/langchain_handler.py
+++ src/server/langchain_handler.py
@@ -3,13 +3,11 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
 import os
-import vertexai
 from langchain_community.document_loaders import S3DirectoryLoader
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.prompts.chat import MessagesPlaceholder
-from langchain_google_vertexai import ChatVertexAI
 from langchain_openai import ChatOpenAI
 from langchain.chains import create_history_aware_retriever
 from langchain_core.runnables.history import RunnableWithMessageHistory
